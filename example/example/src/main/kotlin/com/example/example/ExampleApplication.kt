6939640682313f302ed1c22ade7231d6baa6ad02
Cheel <44915367+miche715@users.noreply.github.com>
2022-06-15 16:25:42 +0900

    Example Init

---
+++ example/example/src/main/kotlin/com/example/example/ExampleApplication.kt
@@ -0,0 +1,12 @@
+package com.example.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExampleApplication
+
+fun main(args: Array<String>)
+{
+    runApplication<ExampleApplication>(*args)
+}
